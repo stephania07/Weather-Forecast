db7b18b3629b7cc87498158742ba72d273bd65c2
Stephania <stephania07>
2014-11-15 18:24:35 -0600

    Added some styles and background image

--- scripts/main.js
+++ scripts/main.js
@@ -6,44 +6,35 @@ document.body.appendChild($script);
 }
 
 function myAwesomeFunction(data) {
-  console.log(data);
   var $ul = document.getElementById("forecast");
   var weather = data;
-    addItemsToList($ul, weather);
+	$city = document.querySelector('.city');
+	$city.innerHTML = weather.location.city;
+	addItemsToList($ul, weather);
 }
 getJSONP(url, 'myAwesomeFunction');
-
-
 
 //creating all the elements that will hold the contents
 function addItemsToList($items, weather) {
   for (var i = 0; i < 5; i++) {
   var $li = document.createElement("li");
-//var $city = document.createElement("p");
   var $conditions = document.createElement("p");
   var $icon_url = document.createElement("img");
   var $title = document.createElement("p");
- // var $fcttext = document.createElement("p"); this text is optional
   var $high = document.createElement("p");
   var $low = document.createElement("p");
- 
 
 //inserting the contents in the created elements
-  //$city.innerHTML = weather.location.city;
     $conditions.innerHTML = weather.forecast.simpleforecast.forecastday[i].conditions;
     $icon_url.src = weather.forecast.simpleforecast.forecastday[i].icon_url;
     $title.innerHTML = weather.forecast.simpleforecast.forecastday[i].date.weekday;
-  //$fcttext.innerHTML = weather.forecast.txt_forecast.forecastday[i].fcttext; //this text is optional
     $high.innerHTML = weather.forecast.simpleforecast.forecastday[i].high.fahrenheit + " "+ "F";
     $low.innerHTML = weather.forecast.simpleforecast.forecastday[i].low.fahrenheit + " " + "F";
-   
 
 //inserting the elements with their children to their parent li
-  //$li.appendChild($city);
-	$li.appendChild($conditions);
+	  $li.appendChild($conditions);
     $li.appendChild($icon_url);
     $li.appendChild($title);
-   // $li.appendChild($fcttext);// this text is optional
     $li.appendChild($high);
     $li.appendChild($low);
 
@@ -52,16 +43,11 @@ function addItemsToList($items, weather) {
   }
 }
 
-/*function currentCity(city) {
-var $cityName = document.getElementById('cityName');
-$cityName.innerHTML = $city;
-}*/
-
  var $form = document.getElementById('zipcode');
- var $zipBox = $form.querySelector('input[type=number]');
+ var $zipBox = $form.querySelector('input[type="number"]');
  $form.addEventListener('submit', function(event){
   event.preventDefault();
-  var url = "http://api.wunderground.com/api/9888e476ac1f7d56/geolookup/forecast10day/q/" + $zipBox.value + '.json';
+  var url = "http://api.wunderground.com/api/9888e476ac1f7d56/geolookup/forecast10day/q/" + $zipBox.value + ".json";
   getJSONP(url, 'myAwesomeFunction');
 });
 
